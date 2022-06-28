dfc746435879b29c1402267d6dbb124970e5e88c
Hyoungnam Kim <kimhyoungnams@gmail.com>
2022-06-28 19:50:50 +0900

    feat: 상품상세 링크 구현

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { numberWithCommas } from 'src/utilities';
 import styled from 'styled-components';
 
 import { Product } from '../types/product';
@@ -6,32 +8,34 @@ type ProductItemProps = {
   product: Product;
 };
 
-const ProductItem = ({ product: { name, thumbnail, price } }: ProductItemProps) => (
-  <Container>
-    <Thumbnail src={thumbnail ? thumbnail : '/defaultThumbnail.jpg'} />
-    <Name>{name}</Name>
-    <Price>{price}</Price>
-  </Container>
+const ProductItem = ({ product: { id, name, thumbnail, price } }: ProductItemProps) => (
+  <Link href={`products/${id}`}>
+    <S_Anchor>
+      <S_Thumbnail src={thumbnail ? thumbnail : '/defaultThumbnail.jpg'} />
+      <S_Name>{name}</S_Name>
+      <S_Price>{numberWithCommas(price)}원</S_Price>
+    </S_Anchor>
+  </Link>
 );
 
 export default ProductItem;
 
-const Container = styled.a`
+const S_Anchor = styled.a`
   width: 180px;
   margin-left: 20px;
   margin-top: 20px;
 `;
 
-const Thumbnail = styled.img`
+const S_Thumbnail = styled.img`
   width: 100%;
   height: 180px;
 `;
 
-const Name = styled.div`
+const S_Name = styled.div`
   margin-top: 8px;
   font-size: 16px;
 `;
 
-const Price = styled.div`
+const S_Price = styled.div`
   margin-top: 4px;
 `;
